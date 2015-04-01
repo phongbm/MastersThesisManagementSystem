b3329359330d3be54cfcc15f1e5d0f04016f7150
Bui Minh Phong <phongbm_58@vnu.edu.vn>
2015-04-01 17:02:42 +0700

    clear code

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -3,16 +3,6 @@
 
 # --- !Ups
 
-create table address (
-  id                        bigint not null,
-  street                    varchar(255),
-  number                    varchar(255),
-  postal_code               varchar(255),
-  city                      varchar(255),
-  country                   varchar(255),
-  constraint pk_address primary key (id))
-;
-
 create table faculty (
   id                        bigint not null,
   name                      varchar(255),
@@ -44,31 +34,16 @@ create table user_account (
   constraint pk_user_account primary key (id))
 ;
 
-create table warehouse (
-  id                        bigint not null,
-  name                      varchar(255),
-  address_id                bigint,
-  constraint pk_warehouse primary key (id))
-;
-
-create sequence address_seq;
-
 create sequence faculty_seq;
 
 create sequence masters_student_seq;
 
 create sequence user_account_seq;
 
-create sequence warehouse_seq;
-
-alter table warehouse add constraint fk_warehouse_address_1 foreign key (address_id) references address (id);
-create index ix_warehouse_address_1 on warehouse (address_id);
 
 
 
 # --- !Downs
-
-drop table if exists address cascade;
 
 drop table if exists faculty cascade;
 
@@ -76,15 +51,9 @@ drop table if exists masters_student cascade;
 
 drop table if exists user_account cascade;
 
-drop table if exists warehouse cascade;
-
-drop sequence if exists address_seq;
-
 drop sequence if exists faculty_seq;
 
 drop sequence if exists masters_student_seq;
 
 drop sequence if exists user_account_seq;
-
-drop sequence if exists warehouse_seq;
 
